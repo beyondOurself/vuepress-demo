0c782289cef5b075160114af61019d3247130d19
longlong <562172151@qq.com>
2022-01-14 17:54:57 +0800

    t2

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,29 @@
+export const data = {
+  "key": "v-8daa1a0e",
+  "path": "/",
+  "title": "",
+  "lang": "zh-CN",
+  "frontmatter": {
+    "home": true
+  },
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "这是首页呀",
+      "slug": "这是首页呀",
+      "children": []
+    }
+  ],
+  "git": {
+    "updatedTime": 1642149173000,
+    "contributors": [
+      {
+        "name": "longlong",
+        "email": "562172151@qq.com",
+        "commits": 1
+      }
+    ]
+  },
+  "filePathRelative": "README.md"
+}
